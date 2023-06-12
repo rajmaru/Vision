ec9a0a7ec1ea8439bb8c5c696c85bf89af8aa38a
Raj Maru <rajmaru0304@gmail.com>
2023-06-12 18:53:50 +0530

    about ui

---
+++ app/src/main/java/com/one/vision/AboutActivity.kt
@@ -0,0 +1,72 @@
+package com.one.vision
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.one.vision.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private var user: FirebaseUser? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+        onClick()
+        getUserFromFirebase()
+        setUserDataInViews()
+
+    }
+
+    private fun init(){
+        mAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun onClick(){
+        binding.aboutLogout.setOnClickListener {
+            logOut()
+        }
+    }
+
+    private fun getUserFromFirebase() {
+        val auth = Firebase.auth
+        user = auth.currentUser
+    }
+
+    private fun setUserDataInViews() {
+        if (user != null) {
+            Glide.with(this)
+                .load(user!!.photoUrl)
+                .into(binding.aboutProfilePic)
+            binding.aboutNameTv.text = user!!.displayName
+            binding.aboutEmailTv.text = user!!.email
+        }
+    }
+
+    private fun logOut() {
+        mAuth.signOut()
+        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+            val intent = Intent(this@AboutActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/one/vision/MainActivity.kt
+++ app/src/main/java/com/one/vision/MainActivity.kt
@@ -1,60 +1,51 @@
 package com.one.vision
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.one.vision.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mGoogleSignInClient: GoogleSignInClient
-    private lateinit var mAuth: FirebaseAuth
+    private var auth: FirebaseAuth? = null
+    private var user: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mAuth = FirebaseAuth.getInstance()
+        init()
+        getUserFromFirebase()
+        setProfilePic()
+        onClick()
+    }
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
+    private fun init(){
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
 
-        val auth = Firebase.auth
-        val user = auth.currentUser
-
-        if (user != null) {
-            val userName = user.displayName
-            binding.email.text = "Welcome, " + userName
-        } else {
-            // Handle the case where the user is not signed in
-        }
-
-        // Inside onCreate() method
-        binding.signOutButton.setOnClickListener {
-            signOutAndStartSignInActivity()
+    private fun onClick() {
+        binding.homeProfilePic.setOnClickListener{
+            startActivity(Intent(this@MainActivity, AboutActivity::class.java))
         }
     }
 
-    private fun signOutAndStartSignInActivity() {
-        mAuth.signOut()
-        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
-            // Optional: Update UI or show a message to the user
-            val intent = Intent(this@MainActivity, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
+    private fun getUserFromFirebase() {
+        auth = Firebase.auth
+        user = auth!!.currentUser
+    }
+
+    private fun setProfilePic() {
+        if (user != null) {
+            Glide.with(this)
+                .load(user!!.photoUrl)
+                .into(binding.homeProfilePic)
         }
     }
 }
